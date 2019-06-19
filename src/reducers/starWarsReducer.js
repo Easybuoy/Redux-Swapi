68e79edd0dff7efe73279c605114c7c00960c422
Ezekiel <ekunolaeasybuoy@gmail.com>
2019-06-19 13:05:48 +0100

    add markup to make redux swapi work

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,6 +1,9 @@
-import /* we need our action types here*/ "../actions";
+import { GET_PEOPLE, FETCHING, GET_ERRORS } from "../actions";
+
 const initialState = {
-  characters: []
+  characters: [],
+  fetching: false,
+  error: null
   // Array characters, Boolean fetching, null error.
 };
 export const charsReducer = (state = initialState, action) => {
@@ -8,6 +11,22 @@ export const charsReducer = (state = initialState, action) => {
     // Fill me in with the important reducers
     // action types should be FETCHING, SUCCESS and FAILURE
     // your switch statement should handle all of these cases.
+    case GET_PEOPLE:
+      return {
+        ...state,
+        characters: action.payload,
+        fetching: false
+      };
+      case FETCHING:
+      return {
+        ...state,
+        fetching: true
+      };
+      case GET_ERRORS:
+      return {
+        ...state,
+        error: action.payload
+      };
     default:
       return state;
   }
--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -3,33 +3,47 @@ import { connect } from "react-redux";
 
 import { CharacterList } from "../components";
 // import actions
+import { getPeople } from "../actions/index";
 
 class CharacterListView extends React.Component {
-  constructor() {
-    super();
-  }
+
 
   componentDidMount() {
     // call our action
+    this.props.getPeople();
   }
 
   render() {
-    if (this.props.fetching) {
-      // return something here to indicate that you are fetching data
+
+    if (this.props.characters.error) {
+      return (
+        <div className="text-center">{this.props.characters.error}</div>
+      )
     }
+
+    if (this.props.characters.fetching) {
+      return (
+        <div className="text-center">Loading</div>
+      )
+    }
+
     return (
       <div className="CharactersList_wrapper">
-        <CharacterList characters={this.props.characters} />
+        <CharacterList characters={this.props.characters.characters} />
       </div>
     );
   }
 }
 
+const mapStateToProps = state => ({
+  characters: state.charsReducer
+});
+
 // our mapStateToProps needs to have two properties inherited from state
 // the characters and the fetching boolean
 export default connect(
-  null /* mapStateToProps replaces null here */,
+  mapStateToProps,
   {
-    /* action creators go here */
+    getPeople
   }
 )(CharacterListView);
